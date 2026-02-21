b0a17a4953d32617cb284a5368d30c9efca309ef
Sigve Nakken <sigven@ifi.uio.no>
2026-02-21 11:44:02 +0100

    Catalog version 2026-02-16

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -6,8 +6,8 @@ source("data-raw/gwas_utils.R")
 
 options(timeout = 5000000)
 
-catalog_version_date <- '2026-01-19'
-ebi_catalog_version_date <- '2026-01-19'
+catalog_version_date <- '2026-02-16'
+ebi_catalog_version_date <- '2026-02-16'
 fname_catalog_associations <-
   file.path(
     "data-raw",
@@ -140,7 +140,7 @@ gwas_hits_pr_rsid[['all']] <- as.data.frame(
   )
 
 
-version_bumped <- "1.5.9"
+version_bumped <- "1.6.0"
 
 for (c in gwas_collections) {
 
